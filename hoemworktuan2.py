eda09b74153a4d064d1e2ca0c8afb3ec015f0931
nguyentuankhang_25021548_lop39 <25021548@vnu.edu.vn>
2025-11-27 09:14:54 +0700

    Add files via upload

--- hoemworktuan2.py
+++ hoemworktuan2.py
@@ -13,3 +13,66 @@ s = (a+b)/2
 print("dien tich tam giac", s) 
 d = a + b + c
 print("chu vi la", d)
+
+a = float(input("nhập số a: "))
+b = float(input("nhập số b: "))
+s=-b/a
+if b != 0:
+    print("ax+b có nghiệm là", '%.2f' % s
+          )
+else:
+    print("phương trình vô nghiệm")
+
+a = float(input("nhập hệ số a: "))
+b = float(input("nhập hệ số b: "))
+c = float(input("nhập hệ số c: "))
+d = b**2 - 4*a*c
+if d < 0:
+    print("phương trình vô nghiệm")
+elif d == 0:
+    x = -b / (2*a)
+    print("phương trình có nghiệm kép x1 = x2 =", '%.2f' % x)
+else :
+     x1 = (-b + d**0.5) / (2*a)
+     x2 = (-b - d**0.5) / (2*a)
+     print("phương trình có hai nghiệm phân biệt:")
+     print("x1 =", '%.2f' % x1)
+     print("x2 =", '%.2f' % x2)
+a = float(input("nhập số a: "))
+b = float(input("nhập số b: "))
+c = float(input("nhập số c: "))
+d = float(input("nhập số d: "))
+số_lớn_nhất = a
+if b > số_lớn_nhất:
+    số_lớn_nhất = b
+if c > số_lớn_nhất:
+    số_lớn_nhất = c
+if d > số_lớn_nhất:
+    số_lớn_nhất = d
+print("Số lớn nhất là:", số_lớn_nhất)
+
+a = float(input("nhập số a: "))
+b = float(input("nhập số b: "))
+c = float(input("nhập số c: "))
+d = float(input("nhập số d: "))
+m = float(input("nhập số m: "))
+n = float(input("nhập số n: "))
+D = (a*d-b*c)
+Dx = (m*d - b*n)
+Dy = (a*n - m*c)
+if D == 0:
+    print("Hệ phương trình vô nghiệm hoặc vô số nghiệm.")
+else:
+    x = Dx / D
+    y = Dy / D
+    print("Nghiệm của hệ phương trình là:")
+    print("x =", '%.2f' % x)
+    print("y =", '%.2f' % y)
+a = float(input("nhập số giây: "))
+giờ = a // 3600
+phút = (a % 3600) // 60
+if a == 0 :
+     print("không có thời gian")
+else : 
+    thời_gian  = giờ + phút
+print("thời gian là:", thời_gian)
